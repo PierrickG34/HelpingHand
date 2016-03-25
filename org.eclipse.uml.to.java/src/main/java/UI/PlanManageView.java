c895954a29134d4c023f5b1974676d29e29c229b
Pierrick Giuliani <giulianipierrick@gmail.com>
2016-03-25 21:36:01 +0100

    Done : UI for manage plan admin

--- org.eclipse.uml.to.java/src/main/java/UI/PlanManageView.java
+++ org.eclipse.uml.to.java/src/main/java/UI/PlanManageView.java
@@ -13,27 +13,79 @@ import javax.swing.JPanel;
 
 import Core.User;
 
-public class DashboardAdminView extends JFrame implements ActionListener {
-	
+public class PlanManageView extends JFrame implements ActionListener {
+
+	/**
+	 * Menu button for dashboard
+	 */
 	JButton dashboard = new JButton("Dashboard");
+	
+	/**
+	 * Menu button for activity category
+	 */
 	JButton activityCategory = new JButton("Activity Category");
+	
+	/**
+	 * Menu button for product category
+	 */
 	JButton productCategory = new JButton("Product Category");
+	
+	/**
+	 * Menu button for product
+	 */
 	JButton product = new JButton("Product");
+	
+	/**
+	 * Menu button for account
+	 */
 	JButton account = new JButton("Account");
+	
+	/**
+	 * Menu button for plan
+	 */
 	JButton plan = new JButton("Plan");
+	
+	/**
+	 * Menu button for task
+	 */
 	JButton task = new JButton("Task");
-	   
+	
+	/**
+	 * Menu button for profile
+	 */   
 	JButton profile = new JButton("Profile");
+	
+	/**
+	 * Menu button for notification
+	 */
 	JButton notifications = new JButton("Notifications");
 	
+	/**
+	 * Button for create an activity category
+	 */
+	JButton create = new JButton("Create a Plan");
 	
+	/**
+	 * Button for update an activity category
+	 */
+	JButton update = new JButton("Update a Plan");
+	
+	JButton delete = new JButton("Delete a Plan");
+	
+	/**
+	 * The current user
+	 */
 	private User currentUser;
+
 	
-	
-	
-	public DashboardAdminView(User currentUser) {
-		super("DashBoard"); // Name of the frame
+	/**
+	 * Constructor for the class
+	 * @param currentUser
+	 */
+	public PlanManageView(User currentUser) {
+		super("Plan"); // Name of the frame
 		this.currentUser = currentUser;
+		
 		/* Defined actions on the different buttons */
 		this.dashboard.addActionListener(this);
 		this.activityCategory.addActionListener(this);
@@ -45,12 +97,14 @@ public class DashboardAdminView extends JFrame implements ActionListener {
 		this.profile.addActionListener(this);
 		this.notifications.addActionListener(this);
 		
+		/*Define the container*/
         Container contentPane = getContentPane(); 
         contentPane.setLayout(new BorderLayout());
         setMinimumSize(new Dimension(1000,500));
         setMaximumSize(new Dimension(1000,500));
         
-        JPanel panelButton = new JPanel(new GridLayout(3, 1)); // 2 rows x 1 column
+        /*Define container for the display*/
+        JPanel panelButton = new JPanel(new GridLayout(2, 1)); // 2 rows x 1 column
         JPanel panelTopButton = new JPanel();
         JPanel panelBottomButton = new JPanel();
         
@@ -68,50 +122,50 @@ public class DashboardAdminView extends JFrame implements ActionListener {
         panelButton.add(panelTopButton);
         panelButton.add(panelBottomButton);
         
-  
         contentPane.add(panelButton,BorderLayout.NORTH);
+        
+  
+        /*-------------- Veritable view --------------------*/
+        JPanel manageButton = new JPanel(new GridLayout(2, 1));
+        JPanel createButton = new JPanel();
+        JPanel updateButton = new JPanel();
+        JPanel deleteButton = new JPanel();
+        
+        this.create.addActionListener(this);
+        this.update.addActionListener(this);
+        this.delete.addActionListener(this);
+        
+        createButton.add(this.create);
+        updateButton.add(this.update);
+        deleteButton.add(this.delete);
+        
+        manageButton.add(createButton);
+        manageButton.add(updateButton);
+        manageButton.add(deleteButton);
+        
+        contentPane.add(manageButton, BorderLayout.WEST);
 
         //Display
         setSize(400,120);
         setVisible(true);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 	}
-
+	
+	/**
+	 * Action when the user click on a button
+	 * @param e
+	 */
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		String source = e.getActionCommand();
-		if (source == "Dashboard") {
-			System.out.println("Je suis Dashboard");
-			System.out.println("Action a définir ici...");
+		if(source == "Create a Plan") {
+			System.out.println("PlanManageView --> DEJA FAIT");
 		}
-		else if (source == "Activity Category" ){
-			ActivtyCategoryManageView activityCategory = new ActivtyCategoryManageView(this.currentUser);
+		else if(source == "Update a Plan") {
+			System.out.println("PlanManageView --> Button udpdate");
 		}
-		else if (source == "Product Category"){
-			System.out.println("Je suis Product Category");
-			System.out.println("Action a définir ici...");
-		}
-		else if (source == "Product"){
-			System.out.println("Je suis Product");
-			System.out.println("Action a définir ici...");
-		}
-		else if (source == "Account"){
-			AccountAdminView accountAdminView = new AccountAdminView(this.currentUser);
-		}
-		else if (source == "Plan"){
-			PlanManageView planManageView = new PlanManageView(this.currentUser);
-		}
-		else if (source == "Task"){
-			TaskManageView taskManageView = new TaskManageView(this.currentUser);
-		}
-		else if (source == "Profile"){
-			System.out.println("Je suis Profile");
-			System.out.println("Action a définir ici...");
-		}
-		else if (source == "Notifications"){
-			System.out.println("Je suis Notifications");
-			System.out.println("Action a définir ici...");
+		else if(source == "Delete a Plan") {
+			System.out.println("PlanManageView --> Delete udpdate");
 		}
 	}
-
 }
