733e2d01707f7700aa7c8a51aa48422d576e3828
dorian <dorian.vacher@gmail.com>
2016-03-29 13:48:18 +0200

    UI - Test menus

--- org.eclipse.uml.to.java/src/main/java/UI/ActivtyCategoryManageView.java
+++ org.eclipse.uml.to.java/src/main/java/UI/ActivtyCategoryManageView.java
@@ -16,51 +16,6 @@ import Core.User;
 public class ActivtyCategoryManageView extends JPanel implements ActionListener {
 
 	/**
-	 * Menu button for dashboard
-	 */
-	JButton dashboard = new JButton("Dashboard");
-	
-	/**
-	 * Menu button for activity category
-	 */
-	JButton activityCategory = new JButton("Activity Category");
-	
-	/**
-	 * Menu button for product category
-	 */
-	JButton productCategory = new JButton("Product Category");
-	
-	/**
-	 * Menu button for product
-	 */
-	JButton product = new JButton("Product");
-	
-	/**
-	 * Menu button for account
-	 */
-	JButton account = new JButton("Account");
-	
-	/**
-	 * Menu button for plan
-	 */
-	JButton plan = new JButton("Plan");
-	
-	/**
-	 * Menu button for task
-	 */
-	JButton task = new JButton("Task");
-	
-	/**
-	 * Menu button for profile
-	 */   
-	JButton profile = new JButton("Profile");
-	
-	/**
-	 * Menu button for notification
-	 */
-	JButton notifications = new JButton("Notifications");
-	
-	/**
 	 * Button for create an activity category
 	 */
 	JButton create = new JButton("Create an Activity Category");
@@ -85,45 +40,6 @@ public class ActivtyCategoryManageView extends JPanel implements ActionListener 
 //		super("Activity Category"); // Name of the frame
 		this.currentUser = currentUser;
 		this.vc = vc;
-		
-		/* Defined actions on the different buttons */
-		this.dashboard.addActionListener(this);
-		this.activityCategory.addActionListener(this);
-		this.productCategory.addActionListener(this);
-		this.product.addActionListener(this);
-		this.account.addActionListener(this);
-		this.plan.addActionListener(this);
-		this.task.addActionListener(this);
-		this.profile.addActionListener(this);
-		this.notifications.addActionListener(this);
-		
-		/*Define the container*/
-//        Container contentPane = getContentPane(); 
-//        contentPane.setLayout(new BorderLayout());
-//        setMinimumSize(new Dimension(1000,500));
-//        setMaximumSize(new Dimension(1000,500));
-        
-        /*Define container for the display*/
-        JPanel panelButton = new JPanel(new GridLayout(2, 1)); // 2 rows x 1 column
-        JPanel panelTopButton = new JPanel();
-        JPanel panelBottomButton = new JPanel();
-        
-        panelTopButton.add(this.dashboard);
-        panelTopButton.add(this.activityCategory);
-        panelTopButton.add(this.productCategory);
-        panelTopButton.add(this.product);
-        panelTopButton.add(this.account);
-        panelTopButton.add(this.plan);
-        panelTopButton.add(this.task);
-        
-        panelBottomButton.add(this.profile);
-        panelBottomButton.add(this.notifications);
-        
-        panelButton.add(panelTopButton);
-        panelButton.add(panelBottomButton);
-        
-//        contentPane.add(panelButton,BorderLayout.NORTH);
-        this.add(panelButton,BorderLayout.NORTH);
   
         /*-------------- Veritable view --------------------*/
         JPanel manageButton = new JPanel(new GridLayout(2, 1));
@@ -142,10 +58,6 @@ public class ActivtyCategoryManageView extends JPanel implements ActionListener 
 //        contentPane.add(manageButton, BorderLayout.WEST);
         this.add(manageButton, BorderLayout.WEST);
 
-//        //Display
-//        setSize(400,120);
-//        setVisible(true);
-//        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 	}
 	
 	/**
@@ -156,11 +68,9 @@ public class ActivtyCategoryManageView extends JPanel implements ActionListener 
 	public void actionPerformed(ActionEvent e) {
 		String source = e.getActionCommand();
 		if(source == "Create an Activity Category") {
-//			CreateActivityCategoryView createActivityCategory = new CreateActivityCategoryView(this.currentUser);
 			this.vc.CreateActivityCategoryView();
 		}
 		else if(source == "Update an Activity Category") {
-//			UpdateActivityCategoryView updateActivityCategory = new UpdateActivityCategoryView(this.currentUser);
 			this.vc.UpdateActivityCategoryView();
 		}
 	}
